db9192831cc686bcd77880024039ced7253bb077
Michael <michael.feldman09@gmail.com>
2025-02-24 12:08:43 -0500

    Rename file to prevent compilation error
    The MichaelAngeloRoomScripter and RoomScripter had the same class name,
    preventing Unity from compiling / running the game.
    
    Changed MichaelAngeloRoomScripter class name to match file name.

--- Assets/Scripts/MichaelAngeloRoomScripter.cs
+++ Assets/Scripts/MichaelAngeloRoomScripter.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoomScripter : MonoBehaviour
+public class MichaelAngeloRoomScripter : MonoBehaviour
 {
     private RoomManager roomManager;
     public Textures textures;
