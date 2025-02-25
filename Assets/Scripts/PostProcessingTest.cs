954a7c5318b51a8892b308d185d1407369403c45
Michael <michael.feldman09@gmail.com>
2025-02-25 11:43:31 -0500

    Switch to deferred lighting
    Since we will likely have many point lights in the scene at once, a
    deferred path may be the only viable option.
    
    Still requires investigation as to how we can adapt Unity's deferred
    lighting to our room-based system.
    
    This commit switches the lighting path, and adds a debugging post
    process shader which lets us confirm that we are indeed using deferred
    lighting by visualizing the g buffer channels.

---
+++ Assets/Scripts/PostProcessingTest.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostProcessingTest : MonoBehaviour
+{
+    public Shader shader;
+    private Material _mat;
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (_mat == null)
+        {
+            _mat = new Material(shader);
+        }
+        
+        Graphics.Blit(src, dest, _mat);
+    }
+}
