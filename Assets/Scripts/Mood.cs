45ae8a53441751e7f61ef3ff818bc3ffc6a8b3f2
Michael <michael.feldman09@gmail.com>
2025-02-26 09:16:05 -0500

    Setup Mood Scriptable Object
    The mood SO will be the data holder where users can define the settings
    that constitute a mood. Starting with ambient color only.

---
+++ Assets/Scripts/Mood.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Mood")]
+public class Mood : ScriptableObject
+{
+    public Color ambientColor;
+}
